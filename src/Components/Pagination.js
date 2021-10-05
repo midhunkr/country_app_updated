c8fa6583f407770aa9cc74761cf6a43c2d6ee519
Midhun <midun.kr@inapp.com>
2021-10-05 17:20:31 +0530

    pagination completed

--- src/Components/Pagination.js
+++ src/Components/Pagination.js
@@ -2,8 +2,9 @@ import { Pagination } from 'react-bootstrap'
 function PaginationFrame(props) {
     const itemLength = props.length;
     const pageNumbers = [];
+    const change=props.change;
     for (let i = 1; i <=itemLength; i++) {
-        pageNumbers.push(<Pagination.Item>{i}</Pagination.Item>)
+        pageNumbers.push(<Pagination.Item onClick={()=>{change(i-1)}}>{i}</Pagination.Item>)
     }
     return (
 
--- src/Pages/Home/Home.js
+++ src/Pages/Home/Home.js
@@ -6,39 +6,78 @@ import { useHistory } from "react-router"
 import CountryCard from "../../Components/CountryCard"
 import DataGrid from "../../Components/DataGrid"
 import PaginationFrame from "../../Components/Pagination"
-import { fetchDataAsync,filterCountryList } from '../../State/Slices/fetchDataSlice'
+import { fetchDataAsync, filterCountryList } from '../../State/Slices/fetchDataSlice'
+var startingIndex = 0;
 function Home() {
     const state = useSelector((state) => state.fetchData)
-    const dispatch=useDispatch()
-    const history=useHistory()
-    const filteredData=state.filteredCountryList;
-    const activeContinent=state.activeContinent;
-    let activeItem=state.continentData[0]
-    const pageLength=Math.ceil(filteredData.length/5);
-   
+    const dispatch = useDispatch()
+    const history = useHistory()
+    const filteredData = state.filteredCountryList;
+    const activeContinent = state.activeContinent;
+    let activeItem = state.continentData[0]
+    const pageLength = Math.ceil(filteredData.length / 5);
+    console.log(filteredData);
+    const [start,setStart]=useState(0)
+    
+    const currentElements = [];
+    let [updatedList,setUpdatedList]=useState([])
     useEffect(() => {
-        if(filteredData.length==0||activeContinent==''){
+        if (filteredData.length == 0 || activeContinent == '') {
             dispatch(fetchDataAsync())
         }
         filterCountries(activeItem)
+        changeItems(1)
     }, []);
     const filterCountries = (item) => {
-        dispatch(filterCountryList({filter:item}))
+        dispatch(filterCountryList({ filter: item }))
+        changeItems(0);
     }
-    if(!state.isLoggedIn){
-        const reRoute=()=>{
+
+    const changeItems = (index) => {
+        console.log(`the idex is ${index}`);
+        let count = 0;
+        console.log('before loop');
+        while (currentElements.length > 0) {
+            console.log('first  loop begins');
+            currentElements.pop()
+
+        }
+        if(index===0){
+            startingIndex=0;
+        }
+        else{
+            startingIndex=(index+1)*5-5;
+        }
+        console.log(`starting index ${startingIndex}`);
+        console.log('first  loop ends');
+        console.log(`index is ${index}`);
+        console.log(`index is ${index}`);
+        while (startingIndex <= filteredData.length - 1 && count != 5) {
+
+            console.log('entered in loop');
+            currentElements.push(filteredData[startingIndex]);
+            startingIndex+=1
+            count += 1;
+        }
+        console.log(currentElements);
+        setUpdatedList(currentElements);
+        updatedList.map((item)=>console.log(item))
+        // console.log(`the updated list ${JSON.stringify(updatedList[0])}`);
+    }
+    if (!state.isLoggedIn) {
+        const reRoute = () => {
             history.push('/register')
         }
-        return(
+        return (
             <Container className="me-3 mt-3" fluid>
                 <Alert variant="danger">
                     You are not Logged In.Please <Alert.Link onClick={reRoute}>Login.</Alert.Link>
-                    
+
                 </Alert>
             </Container>
         )
     }
-    if(filteredData.length==0||activeContinent==''){
+    if (filteredData.length == 0 || activeContinent == '') {
         dispatch(fetchDataAsync())
     }
     return (
@@ -47,24 +86,25 @@ function Home() {
                 <Col md={3}>
                     <ListGroup as="ul" className="w-75 m-3">
                         {state.continentData.map((item) => <ListGroup.Item as="li" key={item} active={activeContinent == item ? true : false}
-                            onClick={() => { 
+                            onClick={() => {
                                 // activeItem=item;
-                                 filterCountries(item); }}
+                                filterCountries(item);
+                            }}
                         >{item}</ListGroup.Item>)}
                     </ListGroup>
                 </Col>
                 <Col md={9}  >
                     <Container>
-                 
-                        <DataGrid data={filteredData} firstButtonName="Favourite" ></DataGrid>
-                        
+
+                        <DataGrid data={updatedList} firstButtonName="Favourite" ></DataGrid>
+
                     </Container>
 
                 </Col>
             </Row>
             <Row>
                 <Col className="d-flex justify-content-center">
-                <PaginationFrame length={pageLength}></PaginationFrame>
+                    <PaginationFrame length={pageLength} change={changeItems}></PaginationFrame>
                 </Col>
             </Row>
         </Container>
